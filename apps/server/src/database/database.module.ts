0f988071a2651c8517c9895113db0af9e6b66828
Khaing Myel Khant <khaingmyelkhant@ucsm.edu.mm>
2023-04-02 14:19:02 +0630

    update: db relations

--- apps/server/src/database/database.module.ts
+++ apps/server/src/database/database.module.ts
@@ -2,6 +2,9 @@ import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '@/users/entities/user.entity';
+import { Collection } from './collections/entities/collection.entity';
+import { Comment } from './shots/entities/comment.entity';
+import { Shot } from './shots/entities/shot.entity';
 
 @Module({
   imports: [
@@ -16,7 +19,7 @@ import { User } from '@/users/entities/user.entity';
         password: configService.get<string>('postgres.password'),
         database: configService.get<string>('postgres.database'),
         synchronize: true,
-        entities: [User],
+        entities: [User, Collection, Comment, Shot],
       }),
     }),
   ],
